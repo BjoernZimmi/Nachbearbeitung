17468aa7a090563c0db7b4d05c34feccc2e35a46
Bjoern Zimmer <bjoern.zimmer@dachpc.com>
2024-05-28 11:35:56 +0200

    added injection component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { routerModule } from '../module/router.module';
+import { ServiceInjectionComponent } from "./service-injection/service-injection.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [routerModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [routerModule, ServiceInjectionComponent]
 })
 export class AppComponent {
   title = 'Nachbearbeitungs App';
---
+++ src/app/service-injection/service-injection.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-service-injection',
+  standalone: true,
+  imports: [],
+  templateUrl: './service-injection.component.html'
+})
+
+export class ServiceInjectionComponent {
+
+}
