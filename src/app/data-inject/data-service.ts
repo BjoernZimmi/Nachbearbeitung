d8b35f5f3a76383b09d0e000c142b20e8a3d2a06
Bjoern Zimmer <bjoern.zimmer@dachpc.com>
2024-05-28 16:41:14 +0200

    remove secend service from dataService

--- src/app/data-inject/data-service.ts
+++ src/app/data-inject/data-service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 
-export class DataServiceService {
+export class DataService {
   private data!: string;
 
   constructor() { }
