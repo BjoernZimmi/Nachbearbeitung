580212d394021ee5652c6e1cbbbd7c44467969eb
Bjoern Zimmer <bjoern.zimmer@dachpc.com>
2024-05-29 12:37:20 +0200

    update service

--- src/app/service-injection/service-injection.component.ts
+++ src/app/service-injection/service-injection.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { InputComponent } from "../data-inject/input/input.component";
 import { LoadComponent } from "../data-inject/load/load.component";
+import { SignalComponent } from "../data-inject/signal/signal.component";
 
 @Component({
     selector: 'app-service-injection',
     standalone: true,
     templateUrl: './service-injection.component.html',
-    imports: [InputComponent, LoadComponent]
+    imports: [InputComponent, LoadComponent, SignalComponent]
 })
 
 export class ServiceInjectionComponent {
