191b1b5f42e8179a49eeaa1c691d9b85263b1f05
Bjoern Zimmer <bjoern.zimmer@dachpc.com>
2024-05-29 12:36:37 +0200

    set interval to signals

--- src/app/data-inject/signal/signal.component.ts
+++ src/app/data-inject/signal/signal.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, effect, runInInjectionContext, signal } from '@angular/core';
+import { DataService } from '../data-service';
 
 @Component({
   selector: 'app-signal',
@@ -7,6 +8,22 @@ import { Component } from '@angular/core';
   templateUrl: './signal.component.html',
   styles: ``
 })
-export class SignalComponent {
+export class SignalComponent implements OnInit {
 
+  dataSignal = signal<string>('');
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    // Initialisiere das Signal mit dem aktuellen Wert
+    this.dataSignal.set(this.dataService.getData());
+
+    // Setup interval, um auf Änderungen zu reagieren
+    setInterval(() => {
+      const newData = this.dataService.getData();
+      if (this.dataSignal() !== newData) {
+        this.dataSignal.set(newData);
+      }
+    }, 1000); // Überprüft alle 1000 Millisekunden (1 Sekunde)
+  }
 }
