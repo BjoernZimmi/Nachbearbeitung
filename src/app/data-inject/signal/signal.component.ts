24fcf4ba629659ed4bdf6e337e2353f4c97f8ff3
Bjoern Zimmer <bjoern.zimmer@dachpc.com>
2024-05-29 11:22:30 +0200

    added signal component

---
+++ src/app/data-inject/signal/signal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signal',
+  standalone: true,
+  imports: [],
+  templateUrl: './signal.component.html',
+  styles: ``
+})
+export class SignalComponent {
+
+}
