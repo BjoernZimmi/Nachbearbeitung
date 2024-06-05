347a86560a13725856a5c0a44709cd3b398c0821
Bjoern Zimmer <bjoern.zimmer@dachpc.com>
2024-06-05 13:11:58 +0200

    added missing chnges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { routerModule } from '../module/router.module';
 import { ServiceInjectionComponent } from "./service-injection/service-injection.component";
 import { DataInjectSignalComponent } from "./data-inject-signal/data-inject-signal.component";
+import { LocalSessionStorageComponent } from "./data-inject/local-session-storage/local-session-storage.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [routerModule, ServiceInjectionComponent, DataInjectSignalComponent]
+    imports: [routerModule, ServiceInjectionComponent, DataInjectSignalComponent, LocalSessionStorageComponent]
 })
 export class AppComponent {
   title = 'Nachbearbeitungs App';
