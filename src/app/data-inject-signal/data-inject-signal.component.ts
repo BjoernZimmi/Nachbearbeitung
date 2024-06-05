820a6c09226b91a2064a33f6e7e4db49791161f4
Bjoern Zimmer <bjoern.zimmer@dachpc.com>
2024-06-05 10:19:05 +0200

    smal style changes

--- src/app/data-inject-signal/data-inject-signal.component.ts
+++ src/app/data-inject-signal/data-inject-signal.component.ts
@@ -1,4 +1,4 @@
-import { Component, NgModuleRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { SignalDataService } from './service/signal-data.service';
 import { FormsModule } from '@angular/forms';
 
